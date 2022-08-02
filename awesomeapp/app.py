280f005dd885cc8a55fd6db161d8273964be4269
Sourcery AI <>
2022-08-02 19:10:51 +0000

    'Refactored by Sourcery'

--- awesomeapp/app.py
+++ awesomeapp/app.py
@@ -7,7 +7,7 @@ redis = Redis(host='database', port=6379)
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello World 3.0! I have been seen {} times.\n'.format(count)
+    return f'Hello World 3.0! I have been seen {count} times.\n'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
